9fc02fcdc04e5d1030844007a147d483b76f1791
jhalljhall <hall.justin@justinhall.com>
2020-11-19 13:33:53 -0500

    adding provisions for Passport and Cors to work together

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Middleware;
 use Closure;
+
 class CorsMiddleware
 {
   /**
@@ -23,9 +24,28 @@ class CorsMiddleware
       return response()->json('{"method":"OPTIONS"}', 200, $headers);
     }
     $response = $next($request);
-    foreach ($headers as $key => $value) {
-      $response->header($key, $value);
+    $IlluminateResponse = 'Illuminate\Http\Response';
+    $SymfonyResopnse = 'Symfony\Component\HttpFoundation\Response';
+    $headers = [
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, PATCH, DELETE',
+        'Access-Control-Allow-Headers' => 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Authorization , Access-Control-Request-Headers',
+    ];
+
+    if($response instanceof $IlluminateResponse) {
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
+        return $response;
     }
+
+    if($response instanceof $SymfonyResopnse) {
+        foreach ($headers as $key => $value) {
+            $response->headers->set($key, $value);
+        }
+        return $response;
+    }
+
     return $response;
   }
 }
